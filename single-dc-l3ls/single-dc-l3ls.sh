a7777a2f50806520670af19aac5a1a50b0c48f42
David Serra <dserra@arista.com>
2025-02-06 12:31:48 -0500

    Updated bash script to reflect the correct paths.

--- single-dc-l3ls/single-dc-l3ls.sh
+++ single-dc-l3ls/single-dc-l3ls.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo containerlab destroy --topo ~/AVD-Examples/single-dc-l3ls/container-labs/clab-topo-AVD-L3LS-LAB
-sudo containerlab deploy --topo ~/AVD-Examples/single-dc-l3ls/container-labs/clab-topo-AVD-L3LS-LAB
+sudo containerlab destroy --topo ~/AVD-Examples/single-dc-l3ls/clab-topo-AVD-L3LS-LAB
+sudo containerlab deploy --topo ~/AVD-Examples/single-dc-l3ls/clab-topo-AVD-L3LS-LAB 
 rm /home/dserra/.ssh/known_hosts
 ansible-playbook -i ~/AVD-Examples/single-dc-l3ls/inventory.yml ~/AVD-Examples/single-dc-l3ls/deploy.yml
 ansible-playbook -i ~/AVD-Examples/single-dc-l3ls/inventory.yml ~/AVD-Examples/single-dc-l3ls/deploy.yml
