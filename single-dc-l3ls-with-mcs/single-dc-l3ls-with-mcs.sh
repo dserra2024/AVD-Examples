9d1fd0fff583ddee2c410574aa7bdd31a1da959f
David Serra <dserra@arista.com>
2025-02-07 06:48:52 -0500

    Commit to test new token

--- single-dc-l3ls-with-mcs/single-dc-l3ls-with-mcs.sh
+++ single-dc-l3ls-with-mcs/single-dc-l3ls-with-mcs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 sudo containerlab destroy --topo ~/AVD-Examples/single-dc-l3ls-with-mcs/clab-topo-AVD-L3LS-LAB-WITH-MCS
 sudo containerlab deploy  --topo ~/AVD-Examples/single-dc-l3ls-with-mcs/clab-topo-AVD-L3LS-LAB-WITH-MCS
 rm /home/dserra/.ssh/known_hosts
